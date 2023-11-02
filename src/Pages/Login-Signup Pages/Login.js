3cc78430cd5bffa9ba947a431ad6213b48ba0113
harshit1142 <harshitpandey98765@gmail.com>
2023-11-02 22:17:44 +0530

    Start: Initals files created

---
+++ src/Pages/Login-Signup Pages/Login.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import classes from "./Login.module.css"
+
+export default function Login() {
+  return (
+    <div>Login</div>
+  )
+}
---
+++ src/Pages/Login-Signup Pages/Signup.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import classes from "./Signup.module.css"
+
+export default function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
