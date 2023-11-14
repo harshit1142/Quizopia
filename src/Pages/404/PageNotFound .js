3e13f590a36e1a2afab91ae5872327208694cb35
sanyaa23 <sanya.20223234@mnnit.ac.in>
2023-11-14 14:15:08 +0530

    404-code

--- src/Pages/404/PageNotFound .js
+++ src/Pages/404/PageNotFound .js
@@ -1,7 +1,11 @@
 import React from 'react'
+import './styles.css'
 
 export default function PageNotFound() {
   return (
-    <div>404</div>
+    <div className='PNF'>
+      <h3>404 Page Not Found</h3>
+      <p>The page you are looking for does not exist.</p>
+    </div>
   )
 }
