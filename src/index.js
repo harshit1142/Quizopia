1c0afe5125253c139937d7196c943631dfa313b4
harshit1142 <harshitpandey98765@gmail.com>
2024-07-04 13:58:48 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
